7c7a70de9909430482eb33e587087421328dc888
Thomas Dolan <tdolan@berkeley.edu>
2025-02-10 16:22:16 -0800

    added post

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Optional
+from uuid import UUID,uuid4
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+class Task(BaseModel):
+    id: Optional[UUID] = None
+    title: str
+    description: Optional[str] = None
+    completed: bool = False
+
+tasks = []
+@app.post("/tasks/",response_model=Task)
+def create_task(task: Task):
+    task.id = uuid4()
+    tasks.append(task)
+    return task
+
+@app.get("/tasks/", response_model=List[Task])
+def read_tasks():
+    return tasks
+        
+class Task(BaseModel):
+    title: str
+    description: str
+
+
 
 if __name__ == "__main__":
     import uvicorn
