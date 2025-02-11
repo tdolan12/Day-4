91b08bfa76dc509558d84c70dcfbbef8ff65b1be
Thomas Dolan <tdolan@berkeley.edu>
2025-02-10 17:20:35 -0800

    added update and delete yo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
 from uuid import UUID,uuid4
@@ -21,7 +21,31 @@ def create_task(task: Task):
 @app.get("/tasks/", response_model=List[Task])
 def read_tasks():
     return tasks
-        
+
+@app.get("/tasks/{task_id}", response_model=Task)
+def read_task(task_id: UUID):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+    raise HTTPException(status_code=404, detail="Task not found")
+
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: UUID, task_update: Task):
+    for idx, task in enumerate(tasks):
+        if task.id == task_id:
+            updated_task = task.copy(update=task_update.dict(exclude_unset=True))
+            tasks[idx] = updated_task
+            return updated_task
+    raise HTTPException(status_code=404, detail="Task not found")
+
+@app.delete("/tasks/{task_id}", response_model=Task)
+def delete_task(task_id: UUID):
+    for idx, task in enumerate(tasks):
+        if task.id == task_id:
+            return tasks.pop(idx)
+    raise HTTPException(status_code=404, detail="Task not found")
+
+    
 class Task(BaseModel):
     title: str
     description: str
